e26ff9319aac83dd1bf6a9e1a022a96e633e8620
Laura <lstorella10@gmail.com>
2020-06-10 20:23:02 +0200

    modifica js

--- js/script.js
+++ js/script.js
@@ -1,18 +1,12 @@
 $(document).ready(function () {
 
-  $('.with-dropdown').mouseenter(
+  $('.with-dropdown').mouseenter (
     function() {
-      $('.with-dropdown .dropdown').removeClass('hide');
-      $(this).children('.dropdown').addClass('show');
+      $('.with-dropdown .dropdown').removeClass('active');
+      $(this).children('.dropdown').addClass('active');
+
     }
   );
-  
-  $('.with-dropdown').mouseleave(
-    function() {
-      $('.with-dropdown .dropdown').removeClass('show');
-      $(this).children('.dropdown').addClass('hide');
-    }
-  )
 
 
 
